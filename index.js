7119d7d9e461fbd925bc78dc569bc95de203cbb0
Mark Lundin <mark@mark-lundin.com>
2015-07-29 15:42:28 +0100

    bundled

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+	
+	triangle : require('a-big-triangle'),
+	buffer : require('gl-buffer'),
+	gl : require('gl-context'),
+	fbo : require('gl-fbo'),
+	geometry : require('gl-geometry'),
+	shader : require('gl-shader'),
+	texture2d : require('gl-texture2d'),
+	vao : require('gl-vao'),
+
+}
